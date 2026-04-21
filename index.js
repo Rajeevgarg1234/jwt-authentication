5a62adb8e71d1bf42c115f597c1994b3574614b5
Rajeev Garg <rajeevgarg982@gmail.com>
2026-04-21 21:01:22 +0530

    implement the jwt

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
 import express from "express";
 import userRouter from "./routes/user.routes.js";
-import db from '../db/index.js'
-import { userTable  ,userSession } from "../db/schema.js";
+import db from './db/index.js'
+import { userTable  ,userSession } from "./db/schema.js";
 import { eq } from "drizzle-orm";
+import jwt from 'jsonwebtoken';
 const app = express();
 const PORT = process.env.PORT ?? 8000;
 
 app.use(express.json());
 app.use(async function (req,res,next) {
-    const sessionId = req.headers['session-id'];
-    if(!sessionId){
+    const tokenHeader = req.headers['authorization'];
+
+    // Header authorization: Bearer <token>
+
+    if(!tokenHeader){
         return next();
     }
 
-    const [data] = await db.select({
-        id: userTable.id,
-        sessionId: userSession.id,
-        userId: userSession.userId,
-        name: userTable.name,
-        email: userTable.email,
-    })
-    .from(userSession)
-    .rightJoin(userTable, eq(userTable.id,userSession.userId))
-    .where((table) => eq(table.sessionId,sessionId));
-
-        if(!data){
-        return next();
+    if(!tokenHeader.startsWith('Bearer')){
+        return res.status(400).json({ error: 'authorization header must start with the header'});
     }
 
-    req.user = data;
+    const token = tokenHeader.split(' ')[1];
+
+    const decoded = jwt.verify(token,process.env.JWT_SECRET);
+
+    req.user = decoded;
     next();
 });
 
